5dcf5228e5f4731d001fc657a03cb173ab225ab4
gonalmiron <gonalmiron.dev@gmail.com>
2023-07-20 15:36:16 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import server from './services/server.js'
 const port = 8080
 
 server.listen(port, () => {
-    console.log(`servidor online escuhando en puerto ${port}`)
+    console.log(`servidor ON escuhando en puerto ${port}`)
 })
